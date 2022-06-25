314caf85b6f84897dd4574fcd3f19e9251e6253a
João Pedro Carvalho <joaopedro.nc@outlook.com>
2022-06-25 18:09:50 -0300

    Aula 108 - Objeto Map Parte 1

---
+++ CursoJavaScript-CBFCursos/aula108-objeto_map_p1.js
@@ -0,0 +1,26 @@
+// Map é um objeto que itera seus elementos na order da inserção - um loop for...of retorna um array de chave-valor para cada iteração.
+
+let produtos = new Map([
+    ['01', 'Mouse'],
+    ['02', 'Teclado'],
+    ['03', 'Monitor'],
+    ['04', 'Gabinete']
+])
+
+// produtos.set('01', 'Mouse') // set é o método que define uma chave e seu valor no objeto Map
+//     .set('02', 'Teclado')
+//     .set('03', 'Monitor')
+//     .set('04', 'Gabinete')
+
+produtos.set('01', 'Memória')
+
+console.log(produtos.size) // Retorna o número de pares chave/valor no objeto Map
+
+// console.log(produtos.get('01')) // get é o método que retorna o valor associado à chave, ou undefined se não há nenhum valor correspondente
+// console.log(produtos.get('02'))
+// console.log(produtos.get('03'))
+// console.log(produtos.get('04'))
+
+for(produto of produtos){
+    console.log(produto[1])
+}
