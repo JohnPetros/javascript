b81fd3d4fc9223cd2686d519aa47afb81fc30cfd
João Pedro Carvalho <joaopedro.nc@outlook.com>
2022-06-25 11:44:29 -0300

    Aula 98 - Operador Spread

---
+++ CursoJavaScript-CBFCursos/aula095-let_const.js
@@ -0,0 +1,11 @@
+let num = 10
+
+{
+    let num = 100 // let é uma declaração de variável que restringe a variável no escopo no qual foi declarada, bem como nos escopos filhos
+    console.log(num)
+}
+
+  console.log(num)
+
+  const numero = 10 // const é uma declaração de variável que impede que a variável tenha seu valor alterado
+  console.log(numero)
