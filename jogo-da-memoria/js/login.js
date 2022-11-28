dbef2ecbecd986795fa021efcda202f6ad8ed1a3
João Pedro <joaopedro.nc@outlook.com>
2022-11-27 23:58:41 -0300

    Jogo da memória

--- jogo-da-memoria/js/login.js
+++ jogo-da-memoria/js/login.js
@@ -2,6 +2,7 @@ const input = document.querySelector(".login__input");
 const button = document.querySelector(".login__button");
 const form = document.querySelector(".login-form");
 
+
 const validateInput = ({ target }) => {
   if (target.value.length > 2) {
     button.removeAttribute("disabled");
